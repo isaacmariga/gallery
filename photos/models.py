daf6968b42398e4795a44c051a603a92a9ce6fc3
isaac mariga <isaacwangombe5@gmail.com>
2022-05-24 14:29:53 +0300

    Runn ing check

--- photos/models.py
+++ photos/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Images(models.Model):
-    '''
-    model to handle images
-    '''
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=80)
     description = models.TextField()
