fd96aeb0d7db6dd4016d4902d0d82d1872964489
isaac mariga <isaacwangombe5@gmail.com>
2022-05-24 14:42:41 +0300

    Adding saving and deleting methods to models

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Images, Categories, Locations
 
 # Create your tests here.
+
+class ImagesTest(TestCase):
+
+    def setUp(self):
+
+        self.new_category = Categories(name='testing')
+        self.new_category.save_category()
+        
+        self.new_location = Locations(city='Nairobi', country='Kenya')
+        self.new_location.save_location()
+        
+        self.new_picture = Images(image_link='images/picture.jpeg', title='Image title', description='sth random', category=self.new_category, location=self.new_location)
+        self.new_picture.save_image()
+        self.another_picture = Images(image_link='images/photo.jpg', title='Another title', description='sth else more random', category=self.new_category, location=self.new_location)
+        self.another_picture.save_image()
