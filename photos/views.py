297e4b318b41b00cea86140cd931d6d10c1f4860
isaac mariga <isaacwangombe5@gmail.com>
2022-05-24 15:28:51 +0300

    Defining gallery views

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Categories, Images, Locations
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
+
+def gallery(request):
+    pictures = Images.get_all()
+    locations = Locations.get_all()
+    categories = Categories.get_all()
+    return render(request, 'photos.html', {'pictures': pictures, 'locations':locations, 'categories':categories})
