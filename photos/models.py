2f414a961cc2f5b25cc329347ddeb5cb63623a7f
isaac mariga <isaacwangombe5@gmail.com>
2022-05-24 14:34:30 +0300

    Addign category anf location models

--- photos/models.py
+++ photos/models.py
@@ -5,4 +5,13 @@ class Images(models.Model):
     image = models.ImageField(upload_to='images/', default='SOME STRING')
     title = models.CharField(max_length=80)
     description = models.TextField()
-    
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE, default=1)
+    location = models.ForeignKey('Locations', on_delete=models.CASCADE, default=1)
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Locations(models.Model):
+    city = models.CharField(max_length=30)
