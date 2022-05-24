f25607c3a7d7889dcfb0693d1dd513aa19c1e7e9
isaac mariga <isaacwangombe5@gmail.com>
2022-05-24 21:43:28 +0300

    Adding search-results url

--- photos/urls.py
+++ photos/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 
 urlpatterns=[
-    url(r'^$',views.gallery, name ='gallery')
+    url(r'^$',views.gallery, name ='gallery'),
+    url(r'^search/', views.search_results, name='search_results'),
+
 ]
 
 if settings.DEBUG:
