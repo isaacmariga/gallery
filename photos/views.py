1555be9db30d56be8c90467c639f34156c6c54b6
isaac mariga <isaacwangombe5@gmail.com>
2022-05-24 21:36:09 +0300

    Adding search views

--- photos/views.py
+++ photos/views.py
@@ -10,3 +10,15 @@ def gallery(request):
     images = Images.get_all()
     locations = Locations.get_all()
     return render(request, 'photos.html', {'images': images, 'locations':locations})
+
+
+def search(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        res = Images.search_image(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message':message, 'results':res})
+    else:
+        message = 'You have not searched any term'
+        return render(request, 'search.html', {'message':message})
