c64eb5879e871dda05f6a16a87c906514850dd09
isaac mariga <isaacwangombe5@gmail.com>
2022-10-26 13:54:41 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -61,6 +61,9 @@ DATABASES['default'].update(db_from_env)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+ALLOWED_HOSTS="*"
+
 # Application definition
 
 INSTALLED_APPS = [
